48e9e20b9a99171c08615e5ce00ddd4d02d70746
Tchyze <154416548+Tchyze@users.noreply.github.com>
2023-12-20 09:55:58 +0100

    Create ADUserToCSV.ps1

---
+++ ADUserToCSV.ps1
@@ -0,0 +1,9 @@
+Import-Module ActiveDirectory
+
+$ExportPath = "C:\Path\to\your.csv"
+$RemoteServer = "RemoteServer"
+$SearchBase = "OU=Your,DC=Search,DC=Base"
+
+$users = Get-ADUser -Filter * -Properties DisplayName, GivenName, SurName, Title, Department, HomePhone, OfficePhone, MobilePhone, Fax, StreetAddress, City, PostalCode, State, Country, Office, Company, wWWHomePage -Server $RemoteServer -SearchBase $SearchBase
+
+$users | Select-Object DisplayName, GivenName, SurName, Title, Department, HomePhone, OfficePhone, MobilePhone, Fax, StreetAddress, City, PostalCode, State, Country, Office, Company, wWWHomePage | Export-Csv -Path $ExportPath -NoTypeInformation
